4f02189d7fb083bf9925dcd2d8df4f898d573f52
Ethan Ruchotzke <35568374+Ruchotzke@users.noreply.github.com>
2022-02-19 19:33:59 -0600

    Crude removal of overlaps

--- MazeRunning/Assets/IrregularMazeGenerator.cs
+++ MazeRunning/Assets/IrregularMazeGenerator.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Networking;
 using Utilities;
 
 public class IrregularMazeGenerator : MonoBehaviour
@@ -15,7 +16,12 @@ public class IrregularMazeGenerator : MonoBehaviour
     [Header("Prefabs")]
     public Waypoint pf_Waypoint;
 
+    [Header("Draw Settings")] 
+    public bool DrawBaseGrid = false;
+    public bool DrawMazeGrid = true;
+
     private List<AdjacencyNode> samples;
+    private Dictionary<AdjacencyNode, List<(AdjacencyNode a, AdjacencyNode b)>> edges;
 
     private void Start()
     {
@@ -24,7 +30,58 @@ public class IrregularMazeGenerator : MonoBehaviour
             new PoissonSampler(new Rect(Boundary.min.x, Boundary.min.z, Boundary.size.x, Boundary.size.z), MinDistance);
 
         samples = sampler.SampleWithAdjacency(Vector2.zero, ConnectionDistance);
-        
+
+        Debug.Log("Starting overlap detection.");
+        int overlapCount = 0;
+        edges = new Dictionary<AdjacencyNode, List<(AdjacencyNode a, AdjacencyNode b)>>();
+        foreach (var sample in samples)
+        {
+            edges.Add(sample, new List<(AdjacencyNode a, AdjacencyNode b)>());
+            List<AdjacencyNode> toRemove = new List<AdjacencyNode>();
+            foreach (var neighbor in sample.Neighbors)
+            {
+                /* check to see if this edge overlaps with any other edges. if it does, ignore it */
+                bool overlapFound = false;
+                foreach (var list in edges.Values)
+                {
+                    foreach (var line in list)
+                    {
+                        /* First, is this line the direct inverse? If so, we want to skip it */
+                        if (line.a == sample || line.a == neighbor)
+                        {
+                            continue;
+                        }
+                        
+                        Vector2 a = new Vector2(sample.position.x, sample.position.z);
+                        Vector2 b = new Vector2(neighbor.position.x, neighbor.position.z);
+                        Vector2 c = new Vector2(line.a.position.x, line.a.position.z);
+                        Vector2 d = new Vector2(line.b.position.x, line.b.position.z);
+                        
+                        overlapFound = Utilities.Utilities.IsLineIntersecting(a, b, c, d, false);
+
+                        if (overlapFound)
+                        {
+                            overlapCount++;
+                            toRemove.Add(neighbor);
+                            break;
+                        }
+                    }
+
+                    if (overlapFound) break;
+                }
+                
+                if(!overlapFound) edges[sample].Add((sample, neighbor));
+            }
+            
+            /* Remove any overlapping neighbors */
+            foreach (var neighbor in toRemove)
+            {
+                sample.Neighbors.Remove(neighbor);
+                neighbor.Neighbors.Remove(sample);
+            }
+        }
+        Debug.Log("Stopping overlap detection. " + overlapCount + " edges overlapping removed.");
+
         /* Do the backtrace */
         DoBacktrace(samples[0]);
     }
@@ -52,6 +109,7 @@ public class IrregularMazeGenerator : MonoBehaviour
             
             /* explore neighbors */
             bool movedToNeighbor = false;
+            Utilities.Utilities.Shuffle(curr.Neighbors);
             foreach (var neighbor in curr.Neighbors)
             {
                 /* If this node isn't in the translator, it's not visited. */
@@ -98,18 +156,25 @@ public class IrregularMazeGenerator : MonoBehaviour
                 Gizmos.DrawSphere(sample.position, 0.1f);
                 
                 /* Draw the connections */
-                // Gizmos.color = Color.green;
-                // foreach (var neighbor in sample.Neighbors)
-                // {
-                //     Gizmos.DrawLine(sample.position, neighbor.position);
-                // }
-                
-                /* Draw the path */
-                Gizmos.color = Color.red;
-                foreach (var neighbor in sample.OpenNeighbors)
+                if (DrawBaseGrid)
                 {
-                    Gizmos.DrawLine(sample.position, neighbor.position);
+                    Gizmos.color = Color.green;
+                    foreach (var neighbor in sample.Neighbors)
+                    {
+                        Gizmos.DrawLine(sample.position, neighbor.position);
+                    }
                 }
+
+                /* Draw the path */
+                if (DrawMazeGrid)
+                {
+                    Gizmos.color = Color.red;
+                    foreach (var neighbor in sample.OpenNeighbors)
+                    {
+                        Gizmos.DrawLine(sample.position, neighbor.position);
+                    }
+                }
+                
             }
         }
     }
